07eaf529be1b6d6a885df4e33159f0b6c543c01d
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2020-11-13 13:33:43 -0300

    Projeto inicial

---
+++ src/test/java/dev/alexandreyoshimatsu/tasks/functional/TasksTest.java
@@ -0,0 +1,14 @@
+package dev.alexandreyoshimatsu.tasks.functional;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TasksTest {
+
+    @Test
+    public void testAmbiente() {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://www.google.com");
+    }
+}
