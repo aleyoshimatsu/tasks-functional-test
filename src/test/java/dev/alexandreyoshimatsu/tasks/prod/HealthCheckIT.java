81ee3747ec30488b78becf8101a561fc632e4830
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2020-11-30 08:44:25 -0300

    Adicionando health check

---
+++ src/test/java/dev/alexandreyoshimatsu/tasks/prod/HealthCheckIT.java
@@ -0,0 +1,33 @@
+package dev.alexandreyoshimatsu.tasks.prod;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HealthCheckIT {
+
+    private static final String URL_SELENIUM_GRID = "http://192.168.15.6:4444/wd/hub";
+    private static final String URL_APP = "http://192.168.15.6:8001/tasks";
+
+    @Test
+    public void healthCheck() throws MalformedURLException {
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL(URL_SELENIUM_GRID), cap);
+        try {
+            driver.navigate().to(URL_APP);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            String version = driver.findElement(By.id("version")).getText();
+            Assert.assertTrue(version.startsWith("build"));
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
